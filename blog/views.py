d343490e5e5350c1f53ddc2b6eb725a8db53376a
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-03-28 00:10:17 +0530

    Added Navbar and Bootstrap

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,33 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author' : 'Rajvardhan',
+        'title'  : 'The very first blog',
+        'content': 'first post content',
+        'date_posted' : '27-03-2020'
+    },
+    {
+        'author': 'Sanskar',
+        'title': 'The very first blog',
+        'content': 'first post content',
+        'date_posted': '27-03-2020'
+    },
+    {
+        'author': 'Yash',
+        'title': 'The very first blog',
+        'content': 'first post content',
+        'date_posted': '27-03-2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Home Page </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> About page </h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
