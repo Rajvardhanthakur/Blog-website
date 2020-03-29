5eb7647075c642b0da0a31973f7f6c4471162143
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-03-29 21:50:36 +0530

    Databse created for posts

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author' : 'Rajvardhan',
-        'title'  : 'The very first blog',
-        'content': 'first post content',
-        'date_posted' : '27-03-2020'
-    },
-    {
-        'author': 'Sanskar',
-        'title': 'The very first blog',
-        'content': 'first post content',
-        'date_posted': '27-03-2020'
-    },
-    {
-        'author': 'Yash',
-        'title': 'The very first blog',
-        'content': 'first post content',
-        'date_posted': '27-03-2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
