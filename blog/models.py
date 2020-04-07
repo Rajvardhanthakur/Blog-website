902cf4b110dabd2564f8e62aed2e40c89b2689cc
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-04-07 23:29:01 +0530

    Post update and delete

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +10,8 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now) 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk' : self.pk})
